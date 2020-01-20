03b4ca4393420cb471d1ef9a4bb05afbcdec89ec
kazuya kawaguchi <kawakazu80@gmail.com>
2020-01-20 10:53:40 +0900

    updates

--- .github/actions/translation-status-action/entrypoint.sh
+++ .github/actions/translation-status-action/entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-output=$( npx vue-i18n-locale-message status --provider=poeditor-service-provider --conf=poeditor-service-provider-conf.json )
+#output=$( npx vue-i18n-locale-message status --provider=poeditor-service-provider --conf=poeditor-service-provider-conf.json )
 result=$?
 echo $result
 if [ "$result" -eq 0 ]; then
   echo 'Translation completed!'
+elif [ "$result" -eq 4 ]; then
+  echo 'Translation WIP!'
+  exit 1
 else
   exit 1
 fi
