e1ad6f392259282d50b915c2535beea77f102bbe
kazuya kawaguchi <kawakazu80@gmail.com>
2020-01-21 16:44:56 +0900

    updates

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -2,7 +2,7 @@ import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
+  it.skip('renders props.msg when passed', () => {
     const msg = 'new message'
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg }
