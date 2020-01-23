4ecdb381c6d1d8ce8a0af22640cbccb8cc81bf33
kazuya kawaguchi <kawakazu80@gmail.com>
2020-01-23 14:24:01 +0900

    i18n (#38)

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,7 +6,7 @@
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
-    <h3>Installed CLI Plugins</h3>
+    <h3>{{ $t('plugin') }}</h3>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
@@ -61,6 +61,7 @@ a {
 {
   "en": {
     "pulugins": "For a guide and recipes on how to configure / customize this project",
+    "plugin": "Installed CLI Plugins",
     "ecosystem": "Ecosystem",
     "links": "Essential Links"
   },
